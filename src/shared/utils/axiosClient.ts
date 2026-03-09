1793a60aba311db9bd2dc4e297b95cc8128a0fc7
SYP CuongMV <maivancuong@syp.vn>
2026-03-09 10:15:26 +0700

    fix: update API port from 5000 to 3000

--- src/shared/utils/axiosClient.ts
+++ src/shared/utils/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://136.110.46.173:5000/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://136.110.46.173:3000/api';
 
 const axiosClient = axios.create({
   baseURL: API_URL,
